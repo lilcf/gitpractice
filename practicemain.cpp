db02eb5eb0f82d5165405c2c2f745ca6cef14a58
lilcf <lilyfarr.caitlin@gmail.com>
2024-08-29 11:35:36 -0400

    second commit

--- practicemain.cpp
+++ practicemain.cpp
@@ -2,6 +2,13 @@
 
 int main(){
     std::cout << "Hello \n";
+    std::cout << "This is changes for the git practice... Look at the 2d array! \n";
+
+    int array[4] = {1, 4, 6, 8};
+
+    for(int i = 0; i<5; i++){
+        std::cout << array[i] << " ";
+    }
 
     return 0;
 }
